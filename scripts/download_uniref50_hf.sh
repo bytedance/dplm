8a760fc1de4450b8f90b4db7617bb30cd189eb7f
wangxinyou.203 <wangxinyou.203@bytedance.com>
2025-05-15 21:02:50 +0800

    add & modify downloading scripts

--- scripts/download_uniref50_hf.sh
+++ scripts/download_uniref50_hf.sh
@@ -1,3 +1,4 @@
 pip install huggingface_hub
+mkdir -p data-bin
 # download uniref50 dataset from huggingface hub
 huggingface-cli download airkingbd/uniref50 --repo-type dataset --local-dir ./data-bin/uniref50_hf
